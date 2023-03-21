7de4b131e342f89d7532e5ba2f2e789574a3f9f5
Benjuardo <107207725+Benjuardo@users.noreply.github.com>
2023-03-21 03:58:10 -0400

    Removed FMOD, Integrated Wwise.
     Added a audio bank loader script, music manager script, and music test event. I added a if statement to the Add() function in the HeliCountHud class that changes the music if its equal to 5 or more. please review for efficiency.

--- Assets/Scripts/Character/Player/PlayerSounds.cs
+++ Assets/Scripts/Character/Player/PlayerSounds.cs
@@ -1,15 +1,7 @@
 using UnityEngine;
 
 public class PlayerSounds : MonoBehaviour {
+
   
-  public void PlayFmodFootstepsEvent()
-  {
-    FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Footsteps", GetComponent<Transform>().position);
-  }
-  
-  public void PlayFmodJumpEvent()
-  {
-    FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Jump", GetComponent<Transform>().position);
-  }
 
 }
