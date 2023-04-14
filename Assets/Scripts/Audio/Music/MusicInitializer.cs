4ae627c5790cab00a02032b8ec06d341cd7a832d
Miguel A. Morales <31873544+kuthian@users.noreply.github.com>
2023-04-13 22:21:08 -0300

    Update MusicInitializer.cs

--- Assets/Scripts/Audio/Music/MusicInitializer.cs
+++ Assets/Scripts/Audio/Music/MusicInitializer.cs
@@ -6,6 +6,11 @@ public class MusicInitializer : MonoBehaviour
 {
   [SerializeField] private AK.Wwise.Event _wwGameStart;
   
+  void Awake()
+  {
+    DontDestroyOnLoad(this.gameObject);
+  }
+
   private void Start()
   {
     _wwGameStart.Post(gameObject);
