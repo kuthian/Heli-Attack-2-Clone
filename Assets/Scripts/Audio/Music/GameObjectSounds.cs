7d6c8e83c1833c15ba27d14baa315422c1c0c624
Miguel A. Morales <31873544+kuthian@users.noreply.github.com>
2023-11-14 23:58:38 -0400

    Adding comments.

--- Assets/Scripts/Audio/Music/GameObjectSounds.cs
+++ Assets/Scripts/Audio/Music/GameObjectSounds.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 
+/// <summary>
+/// Component for triggering Wwise audio events associated with the start and destruction of a GameObject.
+/// </summary>
 public class GameObjectSounds : MonoBehaviour {
   
+  /// <summary>
+  /// Wwise Event triggered when the GameObject starts.
+  /// </summary>
   [SerializeField]
   private AK.Wwise.Event _wwOnStart;
+
+  /// <summary>
+  /// Wwise Event triggered when the GameObject is destroyed.
+  /// </summary>
   [SerializeField]
   private AK.Wwise.Event _wwOnDestroy;
 
+  /// <summary>
+  /// Called when the script is enabled. Triggers the Wwise Event associated with the GameObject's start.
+  /// </summary>
   private void Start()
   {
     if (_wwOnStart.IsValid()) {
@@ -14,6 +27,9 @@ public class GameObjectSounds : MonoBehaviour {
     }
   }
 
+  /// <summary>
+  /// Called when the GameObject is being destroyed. Triggers the Wwise Event associated with the GameObject's destruction.
+  /// </summary>
   private void OnDestroy()
   {
     if (_wwOnDestroy.IsValid()) {
