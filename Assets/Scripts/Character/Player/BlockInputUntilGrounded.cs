fc6e73f258c8d5d765e4398a19c9d10f109a977b
Miguel A. Morales <31873544+kuthian@users.noreply.github.com>
2024-03-14 13:22:20 -0300

    Fixing initial player fall behaviour

--- Assets/Scripts/Character/Player/BlockInputUntilGrounded.cs
+++ Assets/Scripts/Character/Player/BlockInputUntilGrounded.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 public class BlockInputUntilGrounded : MonoBehaviour
 {
     internal PlayerController player;
-    internal __GunController gun;
 
     private void Awake()
     {
         player = GetComponent<PlayerController>();
-        gun = GetComponentInChildren<__GunController>();
     }
 
     void Start()
     {
-        gun.ShootingDisabled = true;
-        gun.gameObject.SetActive(false);
         player.BlockInput();
     }
 
@@ -23,8 +19,6 @@ public class BlockInputUntilGrounded : MonoBehaviour
         if (player.Grounded)
         {
             player.EnableInput();
-            gun.gameObject.SetActive(true);
-            gun.ShootingDisabled = false;
             enabled = false;
         }
     }
